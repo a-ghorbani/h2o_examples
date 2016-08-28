fe3ec375be52ccbc172151b20a7dbd690d327f38
a-ghorbani <a-ghorbani@users.noreply.github.com>
2016-08-28 19:37:43 +0200

    Create plot_prediction_distribution.R
    added plot_prediction_distribution

---
+++ examples/plot_prediction_distribution.R
@@ -0,0 +1,35 @@
+
+plot_prediction_distribution <- function(df, probCol, targetCol, threshold) {
+  # Plots the prediction distribution.
+  #
+  # Args:
+  #   df: A data frame contains prediction of the probabilities and the true,
+  #       target values.
+  #   probCol: Column name of the predicted probabilities.
+  #   targetCol: The true values of the target variable.
+  #   threshold: Threshold for prediction.
+  #
+  # Returns:
+  #   A ggplot object.
+  
+  df$truth  <- df[, targetCol]  
+  df$p1     <- df[, probCol]
+  
+  v <- rep(NA, nrow(df))
+  v <- ifelse(df$p1 >= threshold & df$truth == 1, "TP", v)
+  v <- ifelse(df$p1 >= threshold & df$truth == 0, "FP", v)
+  v <- ifelse(df$p1 < threshold & df$truth == 1, "FN", v)
+  v <- ifelse(df$p1 < threshold & df$truth == 0, "TN", v)
+  
+  df$pred_type <- v
+  
+  p <- ggplot(data=df, aes(x=truth, y=p1)) + 
+    geom_jitter(aes(color=pred_type, size=.2), alpha=0.6, size=1) +
+    geom_violin(aes(x=truth, y=p1), fill=rgb(1,1,1,alpha=0.6), color=NA) + 
+    geom_hline(yintercept=threshold, color="red", alpha=0.6) +
+    scale_color_discrete(name = "type") +
+    labs(title=sprintf("Threshold at %.2f", threshold)) + 
+    xlab("Truth") + ylab("Probability") 
+  
+  return(p)
+}
