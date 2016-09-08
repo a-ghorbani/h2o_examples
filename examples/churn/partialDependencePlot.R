78845ae7b50853854c47ff3e329d2b1597029ab4
Valar <ghorbani59@gmail.com>
2016-09-08 21:44:08 +0200

    refined partialDependencePlot

--- examples/churn/partialDependencePlot.R
+++ examples/churn/partialDependencePlot.R
@@ -9,9 +9,10 @@ partialDependencePlot <- function(model, cols, data_frame, N=20, values = NULL){
   if(ncol > 1 & !is.null(values) & class(values)!='list'){
       stop("If # of cols is bigger than 1 then values should be a list NULL.")
   }
+  types  <- h2o.getTypes(data_frame)
   
   lvalues <- lapply(cols, function(col){
-    type  <- h2o.getTypes(data_frame)[[which( names(data_frame) == col)]]
+    type  <- types[[which( names(data_frame) == col)]]
   
     vals <- values[[col]]
     
@@ -19,7 +20,7 @@ partialDependencePlot <- function(model, cols, data_frame, N=20, values = NULL){
       if(type == "enum" | type == "string"){
         #vals = as.data.frame(h2o.unique(data_frame[,col]))
         tab <- as.data.frame(h2o.table(data_frame[,col]))
-        vals <- tab[order(tab$Count, decreasing = T),][c(1:N),col]
+        vals <- as.character(tab[order(tab$Count, decreasing = T),][c(1:N),col])
       }else if(type == "int" | type == "real"){
         minVal <- min(data_frame[,col], na.rm=TRUE)
         maxVal <- max(data_frame[,col], na.rm=TRUE)
@@ -57,36 +58,39 @@ partialDependencePlot <- function(model, cols, data_frame, N=20, values = NULL){
   origpreds <- h2o.predict(model, data_frame)
   
   dim_ <- sapply(lvalues, length)
-  res <- array(0, dim = dim_, dimnames = lvalues)
-  ff <- function(valsl, colsn, j, res_){
-    if(j == ncol){
-      tempframe <- data_frame
-      for(i in c(1:ncol)){
-        tempframe[,colsn[i]] <- valsl[i]
-      }
-      newpreds <- h2o.predict(model, tempframe)
-      del <- mean(newpreds$p1-origpreds$p1)
-      eval.parent(substitute(res_ <- del))
-    }else{
-      v <- lvalues[[j]]
-      for(ii in c(1:length(v))){
-        ff(valsl = c(valsl, v[ii]), colsn=c(colsn, cols[j]), j+1, res_[v[ii]])  
+  len <- prod(dim_)
+  res <- array(rep(0,len))
+  idx <- rep(1,ncol)
+  dimc_ <- c(1,cumprod(dim_)[1:(ncol-1)])
+  for(i in c(1:len)){
+    rezid=i
+    for(j in c(ncol:1)){
+      idx[j]=floor((rezid-1)/dimc_[j])+1
+      rezid = (rezid-1) %% dimc_[j] +1
+    }
+    tempframe <- data_frame
+    for(ii in c(1:ncol)){
+      type  <- types[[which( names(data_frame) == cols[ii])]]
+      if(type == "enum"){
+        tempframe[,cols[ii]] <- as.h2o(factor(rep(lvalues[[ii]][idx[ii]], nrow(data_frame))))
+      }else{
+        tempframe[,cols[ii]] <- lvalues[[ii]][idx[ii]]
       }
     }
+    newpreds <- h2o.predict(model, tempframe)
+    del <- mean(newpreds[,3]-origpreds[,3])
+
+    res[i] <- del 
   }
-  ff(c(), c(), 0, res[])
-    ===> change ti this
-  result1 <- matrix(result[,5], ncol = 10)
-  result2 <- apply(a4d, 4, `[`, indices)
-  identical(result1, result2)
   
-  
-  p <- plot(values, 
-            deltas,
-            type='l', 
-            main=paste0("Partial dependence plot for ", target_col, " as a function of ", predictor_col), 
-            xlab=predictor_col, 
-            ylab=paste0("delta.",target_col))
-  
-  return(p)
+  res <- array(res, dim = dim_, dimnames = lvalues)
+ # p <- plot(values, 
+ #           deltas,
+ #           type='l', 
+ #           main=paste0("Partial dependence plot for ", target_col, " as a function of ", predictor_col), 
+ #           xlab=predictor_col, 
+ #           ylab=paste0("delta.",target_col))
+ # 
+ # return(p)
+  return(res)
 }
